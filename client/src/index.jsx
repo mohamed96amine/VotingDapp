cca4ad1effd09de3ff16250e1694af737d638df6
Amine <f@t.com>
2023-03-18 22:48:08 +0100

    add .jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+
 import "./styles.css";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+    <App/>
     </BrowserRouter>
   </React.StrictMode>
 );
